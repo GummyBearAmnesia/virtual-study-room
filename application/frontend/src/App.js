70e12765437b240b1aad7ae077c72beaa15af4ba
Natalia Ahsan <ahsannatalia@hotmail.com>
2025-02-17 23:53:38 +0000

    Add base code for Group Study Room UI

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -9,6 +9,7 @@ import Dashboard from "./pages/Dashboard";
 import GroupStudyPage from "./pages/GroupStudyPage";
 import GroupStudyPage from "./pages/GroupStudyPage";
 import MotivationalMessage from "./pages/Motivation";
+import GroupStudyPage from "./pages/GroupStudyPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ToDoList from "./pages/ToDoList";
 import Dashboard from "./pages/Dashboard";
@@ -25,6 +26,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/group-study" element={<GroupStudyPage />} />
         <Route path="/motivational-message" element={<MotivationalMessage />} />
+        <Route path="/group-study" element={<GroupStudyPage />} />
       </Routes>
     </Router>
   );
