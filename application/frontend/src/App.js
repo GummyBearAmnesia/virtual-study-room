d5e060a492835d215db6e05a4a3457b793e30960
Agrima Khare <agrima.khare@kcl.ac.uk>
2025-02-18 17:24:44 +0000

    Complete Motivational Message feature

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./styles/App.css";
+import React from "react";
+import "./styles/App.css";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Welcome from "./pages/Welcome";
 import Dashboard from "./pages/Dashboard";
 import GroupStudyPage from "./pages/GroupStudyPage";
-import MotivationalMessage from "./pages/Motivation";
 import GroupStudyPage from "./pages/GroupStudyPage";
+import MotivationalMessage from "./pages/Motivation";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ToDoList from "./pages/ToDoList";
 import Dashboard from "./pages/Dashboard";
@@ -22,7 +24,7 @@ function App() {
         <Route path="/motivational-message" element={<MotivationalMessage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/group-study" element={<GroupStudyPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/motivational-message" element={<MotivationalMessage />} />
       </Routes>
     </Router>
   );
