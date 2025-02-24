06b1bbf9aa54b88feef328e602fd354414172f39
Agrima Khare <agrima.khare@kcl.ac.uk>
2025-02-24 13:56:12 +0000

    Finally this branch is free of errors

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -8,7 +8,6 @@ import GroupStudyPage from "./pages/GroupStudyPage";
 import MotivationalMessage from "./pages/Motivation";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ToDoList from "./pages/ToDoList";
-import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -21,9 +20,6 @@ function App() {
         <Route path="/motivational-message" element={<MotivationalMessage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/group-study" element={<GroupStudyPage />} />
-        <Route path="/motivational-message" element={<MotivationalMessage />} />
-        <Route path="/group-study" element={<GroupStudyPage />} />
-        <Route path="/motivational-message" element={<MotivationalMessage />} />
       </Routes>
     </Router>
   );
