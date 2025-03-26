89daa0ff883039d21337d8bac4d06e38a83fe5f8
Agrima Khare <agrima.khare@kcl.ac.uk>
2025-03-26 20:40:45 +0000

    change asgi.py for deployment

--- application/backend/asgi.py
+++ application/backend/asgi.py
@@ -1,10 +1,14 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from api.routing import websocket_urlpatterns
+
 
 # Set default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from api.routing import websocket_urlpatterns
 
 # ASGI application router that handles both HTTP and WebSocket protocols
 application = ProtocolTypeRouter({
